7aac8842ce3cf88e3208bb0c608a9e2e271330db
Hyun Jun Eom <zzp1318@gmail.com>
2021-05-24 21:55:17 +0900

    hhhhh

--- srcs/parse_info/parse_rgb.c
+++ srcs/parse_info/parse_rgb.c
@@ -6,7 +6,7 @@
 /*   By: heom <heom@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/29 19:25:32 by heom              #+#    #+#             */
-/*   Updated: 2021/05/24 16:03:26 by heom             ###   ########.fr       */
+/*   Updated: 2021/05/24 21:19:34 by heom             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,6 @@ int			parse_rgb(char *line, t_info *parse_info, int start)
 {
 	int		i;
 	int		j;
-	int		res;
 
 	i = 0;
 	j = 0;
@@ -38,11 +37,12 @@ int			parse_rgb(char *line, t_info *parse_info, int start)
 			i++;
 			j++;
 		}
+		else
+			return (0);
 	}
 	j = i;
 	i = remove_space(line, i);
 	if (line[i] != '\0')
 		return (0);
-	res = make_rgb(line, parse_info, start, j);
-	return (res);
+	return (make_rgb(line, parse_info, start, j));
 }
